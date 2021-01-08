4e1ff134a1f1d0f6ba4b86843932b42c6937c264
jmanuellh <manuellh2008@gmail.com>
2021-01-08 12:03:06 -0600

    agregado modelo y ruta productos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListaProductoComponent } from "./producto/lista-producto/lista-producto.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'productos',
+    component: ListaProductoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaProductoComponent } from './producto/lista-producto/lista-producto.component';
+import { ApiService } from "./api.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaProductoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
