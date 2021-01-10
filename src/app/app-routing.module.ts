8b71ca11ca0573c88708f651769111ac39365995
jmanuellh <manuellh2008@gmail.com>
2021-01-10 10:12:02 -0600

    agregado menu lateral de pagina

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from "./inicio/inicio.component";
 import { ProductoComponent } from "./producto/component/producto/producto.component";
 
 const routes: Routes = [
   {
+    path: 'inicio',
+    component: InicioComponent
+  },
+  {
     path: 'productos',
     component: ProductoComponent
+  },
+  {
+    path: '',
+    redirectTo: 'inicio',
+    pathMatch: 'full'
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,39 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from "./inicio/inicio.component";
 import { ProductoComponent } from "./producto/component/producto/producto.component";
+
 import { ProductoService } from "./producto/service/producto.service";
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatListModule } from "@angular/material/list";
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    InicioComponent,
     ProductoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule
+  ],
+  exports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [
     ProductoService
